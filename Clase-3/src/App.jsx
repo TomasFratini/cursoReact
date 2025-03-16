68c496971378406f748a0cac55b14ab9a6158131
Tomas Fratini <tomas.fratini09@gmail.com>
2025-03-16 21:26:19 +0100

    prueba tecnica

--- Clase-3/src/App.jsx
+++ Clase-3/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_URL_FACT_CATS = 'https://catfact.ninja/fact'
+
+export function App () {
+  const [fact, setFact] = useState()
+
+  const [imgurl, setImgurl] = useState()
+
+  useEffect(() => {
+    fetch(API_URL_FACT_CATS)
+      .then(response => response.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const firstWord = fact.split(' ', 3).join(' ')
+
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImgurl(url)
+      })
+  }, [fact])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Prueba tecnica </h1>
+      {fact && <p>{fact}</p>}
+      {imgurl && <img src={imgurl} alt={`Img extracted using the firsr word of ${fact}`} />}
+    </main>
   )
 }
 
